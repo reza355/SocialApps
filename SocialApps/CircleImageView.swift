d98d68f060f7940dbfb71cf9ffdafeab9907e0bf
Fathureza Januarza <fathurezajanuarza@gmail.com>
2016-11-22 14:47:06 +0700

    feed complete

---
+++ SocialApps/CircleImageView.swift
@@ -0,0 +1,29 @@
+//
+//  CircleImageView.swift
+//  SocialApps
+//
+//  Created by Fathureza Januarza on 11/22/16.
+//  Copyright © 2016 tedihouse. All rights reserved.
+//
+
+import UIKit
+
+class CircleImageView: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.shadowColor = UIColor(colorLiteralRed: Float(SHADOW_GRAY), green: Float(SHADOW_GRAY), blue: Float(SHADOW_GRAY), alpha: 0.6).cgColor
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSize(width: 0.1, height: 0.1)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+        
+    }
+
+}
